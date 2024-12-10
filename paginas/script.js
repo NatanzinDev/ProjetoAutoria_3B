e11434948108ebefd7020ae273c9617f77b5b859
Natan Paulo <natanpaulo1300@gmail.com>
2024-12-10 11:44:43 -0300

    função de adição javascript

--- paginas/script.js
+++ paginas/script.js
@@ -1,5 +1,10 @@
 function somar(){
+  
+    let quantidade = document.querySelector("#quantidade")
 
+    let a = Number(quantidade.value)
+    a = a+1
+    quantidade.value = a
 }
 
 function subtrair(){
