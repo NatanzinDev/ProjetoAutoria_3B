4b3fbcf40f577772c44bc0b60feedebcc921d3c7
Natan Paulo <natanpaulo1300@gmail.com>
2024-12-11 02:18:15 -0300

    Validando formulario

--- paginas/script.js
+++ paginas/script.js
@@ -1,6 +1,10 @@
-let quantMamao
-let quantMilho
-let quantAbobora 
+let quantmamao = 0
+let quantmilho = 0
+let quantabobora = 0
+let o 
+const form = document.querySelector('#form')
+let quantidade = document.querySelector("#quantidade")
+let carrinho = document.querySelector(".carrinho")
 const nome = document.querySelector("#inome")
 const sobrenome = document.querySelector("#inome")
 const cep = document.querySelector("#icep")
@@ -19,16 +23,16 @@ const telefone = document.querySelector("#itelefone")
 // função para adição de produtos
 function somar(){
   
-    let quantidade = document.querySelector("#quantidade")
+    
 
-    let a = Number(quantidade.value)
+    let a= Number(quantidade.value)
     a = a+1
     quantidade.value = a
 }
 
 // função para subtrair produtos
 function subtrair(){
-    let quantidade = document.querySelector("#quantidade")
+   
     let a = Number(quantidade.value)
 
     if(quantidade.value > 0){
@@ -39,77 +43,123 @@ function subtrair(){
 
 // funções para salvar as quantidades dos produtos
 function salvarCompraMamao(){
-    let quantidade = document.querySelector("#quantidade")
+    let aux = Number(quantidade.value)
+    quantmamao = aux
 
-    quantMamao = Number(quantidade.value)
+    if(quantmamao > 0){
+        window.location.href = "compra.html"
+    }else{
+        alert("A quantidade tem que ser maior que 0.")
+    }
     
 }
 
 function salvarCompraMilho(){
-    let quantidade = document.querySelector("#quantidade")
-
-    quantMilho = Number(quantidade.value)
+    let aux = Number(quantidade.value)
+    quantmilho = aux
+  
+    if(quantmilho > 0){
+        window.location.href = "compra.html"
+    }else{
+        alert("A quantidade tem que ser maior que 0.")
+    }
     
 }
 
-function salvarCompraMamao(){
-    let quantidade = document.querySelector("#quantidade")
-
-    quantAbobora = Number(quantidade.value)
-   
+function salvarCompraAbobora(){
+    let aux = Number(quantidade.value)
+    quantabobora = aux
+    
+    if(quantabobora > 0){
+        window.location.href = "compra.html"
+    }else{
+        alert("A quantidade tem que ser maior que 0.")
+    }
 }
 
 // função para carregar o carrinho
-function carrinho(){
-    if(quantMamao > 0){
-        for(let i = 0; i < quantMamao;i++){
 
-        }
+function carregar(){
+   console.log(quantmilho)
+   console.log(o)
+    if(quantabobora.value > 0 ||quantmamao.value > 0 ||quantmilho > 0){
+        carrinho.innerHTML = `fnejwknf`
     }
 
-    if(quantMilho > 0){
-        for(let i = 0; i < quantMilho;i++){
-
-        }
-    }
-
-    if(quantAbobora > 0){
-        for(let i = 0; i < quantMilho;i++){
-
-        }
-    }
-}
+} 
 
 //função para validar formulário
-function validar(){
+form.addEventListener("submit", (event) =>{
+    event.preventDefault;
+
     if(nome.value === ""){
-        texto.innerHTML = `Preecha seu nome`
+        alert(`Preencha seu nome`)
+        return
     }
 
     if(sobrenome.value === ""){
-       texto.innerHTML = `Preencha o sobrenome`
+        alert(`Preencha seu sobrenome`)
+        return
+    }
+
+    if(email.value ===""  || !validarEmail(email.value)){
+        alert(`Preencha seu email`)
+        return
+    }
+
+    if(endereco.value.length < 5 || endereco.value === ""){
+        alert(`Endereco muito curto ou vazio, preencha corretamente`)
+        return
     }
 
     if(cep.value === ""){
-        texto.innerHTML = `Preecha o cep`
+        alert(`Preencha seu cep `)
+        return
     }
 
-    if(endereco.value === ""){
-        texto.innerHTML = `Preencha seu endereço`
+    if(cpf.value === "" || cpf.value.length != 11){
+        alert("Preencha com um cpf válido")
+        return
     }
 
     if(numero.value === ""){
-        texto.innerHTML = `Preencha o número`
+        alert(`Preencha o número`)
     }
 
-    if(email.value === ""){
-        texto.innerHTML = "Preencha o email"
-    }
-
-    if(cpf.value){
-        texto.innerHTML = `Preencha o cpf`
+    if(telefone === ""){
+        alert(`Preencha com seu telefone`)
     }
 
 
-    
+
+
+
+    form.submit();
+    info()
+});
+
+function validarEmail(e){
+    const eRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    )
+
+    if(eRegex.test(e)){
+        return true
+    }
+
+    return false
+}
+
+const modal = document.querySelector("dialog")
+//informações do comprador
+function info(){
+    let div = document.querySelector(".dialogdiv")
+    let op = document.createElement('p')
+    op.innerHTML = `Nome: ${nome.value} \n Sobrenome: ${sobrenome.value} \n CPF: ${cpf.value} \n Endereço ${endereco.value} \n CEP: ${cep.value} \n Telefone: ${telefone.value} `
+    div.appendChild(op)
+    modal.showModal()
+}
+
+function fechar(){
+    modal.close()
 }
